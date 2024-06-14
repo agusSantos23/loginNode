c56acca6c20d1ec38ddbab37610eaa7712ff8d6c
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-06-14 23:38:04 +0200

    1.4

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 export const {
     PORT = 3000,
-    SALT_ROUNDS = 10
+    SALT_ROUNDS = 10,
+    SECRET_KEY = 'ejEmplodEPrueba_PaRa.El=LoginNode'
 } = process.env
--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
-import { SALT_ROUNDS } from './config.js'
 import { createClient } from "@libsql/client"
 import dotenv from 'dotenv'
+import { SALT_ROUNDS, SECRET_KEY } from './config.js'
 import { v4 as uuidv4 } from 'uuid'
-
 
 dotenv.config()
 
-export const turso = createClient({
+
+const turso = createClient({
     url: process.env.TURSO_DATABASE_URL,
     authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-await turso.execute(`
+
+turso.execute(`
     CREATE TABLE IF NOT EXISTS users( 
     id PRIMARY KEY, 
     name VARCHAR NOT NULL,
@@ -23,16 +26,38 @@ await turso.execute(`
     email VARCHAR NOT NULL
 )`)
 
+
 const app = express();
+
+app.set('view engine','ejs')
 
 const port = process.env.PORT ?? 3000
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.use((req,res,next)=>{
+    const token = req.cookies.access_token
+    
+    if(!token){
+        return res.status(403).redirect('/')
+    }
+
+    try{
+
+        const data = jwt.verify(token, SECRET_KEY)
+        req.session =  req.session ||{}
+        req.session.user = data
+
+    }catch (error) {
+        return res.status(401).redirect('/')
+    }
+    next()
+})
 
 app.get('/',(req,res)=>{
 
-    res.sendFile(process.cwd() + "/client/index.html")
+    return res.render('login')
 })
 
 app.post('/',async(req, res)=>{
@@ -45,34 +70,42 @@ app.post('/',async(req, res)=>{
             sql: "SELECT * FROM users WHERE name = ?",
             args: [username]
         })
-
-        const user = response.rows[0]
-            
+        const user = response.rows[0]         
         if(user){
             const resultHash = await bcrypt.compare(password, user.pass)
             if(resultHash){
-                return res.json({success: true, message: "Has iniciado sesion correctamente"})
+
+                const token = jwt.sign(
+                    {id: user.id, username: user.name},
+                    SECRET_KEY,
+                    {
+                    expiresIn:'1h'
+                    }
+                )
+
+                return res
+                        .cookie('access_token', token,{
+                            httpOnly: true, //La cookie solo se puede adceder desde el servidor
+                            secure: process.env.NODE_ENV == 'production', //La cookie solo puede ser acceder en https
+                            sameSite: 'strict', //solo puede adceder al dominio en el que se encuentra
+                            maxAge: 1000 * 30 * 30 //tiempo de vida
+                        })
+                        .json({success: true, message: "Redireccion"})
             }
         }
-
         return res.json({success: false, message: "No se a encontrado ningun usuario"})
-
-        
-        
+      
     } catch (error) {
 
         console.log(error);
         return res.status(500).json({ success: false, message: "Ocurrió un error en el servidor" });
 
     }
-
 })
-
 app.get('/crear',(req,res)=>{
 
     res.sendFile(process.cwd() + "/client/crearUser.html")
 })
-
 app.post('/crear', async (req,res)=>{
     const {username, password, email} = req.body
 
@@ -90,7 +123,7 @@ app.post('/crear', async (req,res)=>{
             return res.json({success: false, message: "Ya existe un usuario con ese nombre o direcion de correo"})
         } 
 
-        const passwordHash = bcrypt.hashSync(password, SALT_ROUNDS)
+        const passwordHash = await bcrypt.hash(password, SALT_ROUNDS)
 
         const responseNew = await turso.execute({
             sql:"INSERT INTO users (id,name,pass,email)VALUES(?,?,?,?)",
@@ -105,11 +138,24 @@ app.post('/crear', async (req,res)=>{
         
     }catch(e){
 
-        console.log(e)
         return res.status(500).json({ success: false, message: "Ocurrió un error en el servidor" });
     }
 
 })
+app.get('/home',async (req, res) => {
+    try {
+        const {user} = req.session
+        if(!user) return res.status(401).redirect('/')
+       
+        console.log(user)
+        res.render('home', user)
+
+    } catch (error) {
+        console.error('Error verificando token:', error)
+        return res.status(401).redirect('/')
+    }
+});
+
 
 
 app.listen(port,()=>{
